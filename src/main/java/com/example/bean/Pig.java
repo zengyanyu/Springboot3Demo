a0c75cddc1a1ae93ff28d44500ce2a5e8f83d7cc
1194314874@qq.com <zeng123456>
2025-08-13 10:23:17 +0800

    添加文件

--- src/main/java/com/example/bean/Pig.java
+++ src/main/java/com/example/bean/Pig.java
@@ -1,8 +1,12 @@
 package com.example.bean;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@Getter
+@Setter
 @Component
 @ConfigurationProperties(prefix = "pig")
 public class Pig {
@@ -12,30 +16,6 @@ public class Pig {
     private String name;
 
     private Integer age;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
--- src/main/java/com/example/bean/Sheep.java
+++ src/main/java/com/example/bean/Sheep.java
@@ -1,7 +1,11 @@
 package com.example.bean;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "sheep")
 public class Sheep {
 
@@ -10,30 +14,6 @@ public class Sheep {
     private String name;
 
     private Integer age;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 
     @Override
     public String toString() {
