d964c8685ba96860e656a6a48670d46a6cf0fc14
1194314874@qq.com <zeng123456>
2025-08-15 09:53:15 +0800

    优化

--- src/main/java/com/example/bean/Person.java
+++ src/main/java/com/example/bean/Person.java
@@ -2,19 +2,25 @@ package com.example.bean;
 
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
+@Schema(name = "Person对象")
 @JacksonXmlRootElement // 可以写出为xml文档
 @Getter
 @Setter
 public class Person {
 
+    @Schema(description = "主键ID")
     private Long id;
 
+    @Schema(description = "用户名称")
     private String username;
 
+    @Schema(description = "邮箱")
     private String email;
 
+    @Schema(description = "年龄")
     private Integer age;
 }
